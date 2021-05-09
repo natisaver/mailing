340ddf5a175c6c51aaeceafd26c005a27d07379c
nat <64686163+natisaver@users.noreply.github.com>
2021-05-09 13:17:10 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var https = require("https");
+var path = require("path");
+
+var app = express();
+
+app.use(express.static('/public')); //indicates the directory for static items e.g css
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.listen(3000, function(){
+    console.log('server is running on port 3k');
+})
