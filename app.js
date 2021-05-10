182d42146e5a4a6eb46a27c67fb4afa07743a881
nat <64686163+natisaver@users.noreply.github.com>
2021-05-11 02:03:51 +0800

    added success.html and failure.html

--- app.js
+++ app.js
@@ -60,6 +60,10 @@ app.post("/", function(req,res){
     request.end();
 });
 
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
 app.listen(3000, function(){
     console.log('server is running on port 3k');
 })
